11b611e9e1fc10ded49459f1ba521a7daf6a1239
gcmartin <gscalica@gmail.com>
2020-06-27 15:16:06 -0700

    Change links to react-bootstrap nav

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
   return (
     <Container className='container'>
-      
-        <h1>Name</h1>
-        
-        {/* Image from:  https://randomuser.me/photos */}
-        <Image src="https://randomuser.me/api/portraits/lego/5.jpg" roundedCircle />
-        
-      
-        <h2><Link to="/about">About</Link></h2>
-        <h2><Link to="/work">Work</Link></h2>
-        <h2><Link to="/contact">Contact</Link></h2>
-      
+
+      <h1>Name</h1>
+
+      {/* Image from:  https://randomuser.me/photos */}
+      <Image src="https://randomuser.me/api/portraits/lego/5.jpg" roundedCircle />
+
+
+      <Nav className="justify-content-center">
+        <Nav.Item>
+          <Nav.Link>
+            <Link to='/about'>About</Link>
+          </Nav.Link>
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link>
+            <Link to="/work">Work</Link>
+          </Nav.Link> 
+        </Nav.Item>
+
+        <Nav.Item>
+          <Nav.Link>
+            <Link to="/contact">Contact</Link>
+          </Nav.Link> 
+        </Nav.Item>
+      </Nav>
+
     </Container>
-    
+
   );
 }
 
