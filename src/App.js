53be1432df1cd77493eb9d4e6e1716bff2411dbd
gcmartin <gscalica@gmail.com>
2020-06-27 13:31:52 -0700

    Add react-router-dom dependency and create links on Home page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Work from './pages/Work';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <About />
-      <Contact />
-      <Home />
-      <Work />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/work' component={Work} />
+      </div>
+    </Router>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <h1>This is the Home page.</h1>
-  )
+    <div>
+      <h1>Name</h1>
+      <h2>Image will go here</h2>
+      
+      <h2><Link to="/about">About</Link></h2>
+      <h2><Link to="/work">Work</Link></h2>
+      <h2><Link to="/contact">Contact</Link></h2>
+    </div>
+  );
 }
 
 export default Home;
