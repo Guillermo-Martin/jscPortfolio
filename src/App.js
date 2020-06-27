7f911130974f7673fbfd0233cd4767f0c57f9c67
gcmartin <gscalica@gmail.com>
2020-06-26 17:32:07 -0700

    Create Pages folder with components for each page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Work from './pages/Work';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>TEST</h1>
+      <About />
+      <Contact />
+      <Home />
+      <Work />
     </div>
   );
 }
---
+++ src/pages/Work.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Work() {
+  return (
+    <h1>This is the Work page.</h1>
+  )
+}
+
+export default Work;
