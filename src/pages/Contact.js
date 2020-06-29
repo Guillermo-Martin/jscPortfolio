5cefef19098d5ba41f272e57be4a647c4d9f9d44
gcmartin <gscalica@gmail.com>
2020-06-29 16:59:06 -0700

    Create layout for Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Navbar from './../components/Navbar';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 function Contact() {
   return (
-    <h1>This is the Contact page.</h1>
+    <Container>
+      <Navbar />
+
+      <Form>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows="3" />
+        </Form.Group>
+      </Form>
+    </Container>
+
   )
 }
 
