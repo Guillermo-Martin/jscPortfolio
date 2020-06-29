d6750efe624dcb96c6d753acb8d98a11553eceac
gcmartin <gscalica@gmail.com>
2020-06-29 16:39:45 -0700

    Create layout for Work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import Navbar from './../components/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
-function Work() {
+
+function About() {
   return (
-    <h1>This is the Work page.</h1>
+    <Container>
+      
+      <Navbar />
+     
+      <Row>
+        <Col>
+          <p>Illustrations</p>
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+        </Col>
+
+        <Col>
+          <p>Graphic Design</p>
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+          {/* <Col xs={3} md={2}> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+          {/* </Col> */}
+            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
 
-export default Work;
+export default About;
