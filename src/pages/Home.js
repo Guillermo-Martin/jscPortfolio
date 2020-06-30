5e89cf823c3f000404f370e89de13066d203f6d2
gcmartin <gscalica@gmail.com>
2020-06-29 18:20:40 -0700

    Move 'Name' position on Home page and center page components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,39 +3,41 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
+import './../assets/styles/styles.css';
 
 function Home() {
   return (
-    <Container className='container'>
+    
+      <Container className='container Home-container'>
+        {/* Image from:  https://randomuser.me/photos */}
+        <Image src="https://randomuser.me/api/portraits/lego/5.jpg" roundedCircle />
 
-      <h1>Name</h1>
+        <h1>Name</h1>
 
-      {/* Image from:  https://randomuser.me/photos */}
-      <Image src="https://randomuser.me/api/portraits/lego/5.jpg" roundedCircle />
+        <Nav className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to='/about'>About</Link>
+            </Nav.Link>
+          </Nav.Item>
 
 
-      <Nav className="justify-content-center">
-        <Nav.Item>
-          <Nav.Link>
-            <Link to='/about'>About</Link>
-          </Nav.Link>
-        </Nav.Item>
-      
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/work">Work</Link>
+            </Nav.Link>
+          </Nav.Item>
 
-        <Nav.Item>
-          <Nav.Link>
-            <Link to="/work">Work</Link>
-          </Nav.Link> 
-        </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/contact">Contact</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
 
-        <Nav.Item>
-          <Nav.Link>
-            <Link to="/contact">Contact</Link>
-          </Nav.Link> 
-        </Nav.Item>
-      </Nav>
+      </Container>
 
-    </Container>
+
 
   );
 }
