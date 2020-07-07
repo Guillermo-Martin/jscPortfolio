401567c1839272d6cb58cac636b0d00ed735084f
gcmartin <gscalica@gmail.com>
2020-07-06 17:43:30 -0700

    Create 'images' folder, change images to links, style thumbnail

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import burger from './../assets/images/burger.jpeg';
 
 
 function Work() {
@@ -17,41 +18,49 @@ function Work() {
         <Row>
           <Col>
             <p className="Work-header">Illustrations</p>
-            {/* <Col xs={3} md={2}> */}
-            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
-            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
-            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
-            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
-            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+            
+            {/* Image from:  https://unsplash.com/ */}
+
+            <a href={burger}>
+              <Image src={burger} thumbnail className="Work-image" />
+            </a>
+            
+            
+            <a href="https://randomuser.me/api/portraits/men/88.jpg">
+              <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+            </a>
+            
+            <a href="https://randomuser.me/api/portraits/men/88.jpg">
+              <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+            </a>
+            
+            <a href="https://randomuser.me/api/portraits/men/88.jpg">
+              <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+            </a>
+            
+            <a href="https://randomuser.me/api/portraits/men/88.jpg">
+              <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+            </a>
+            
+            <a href="https://randomuser.me/api/portraits/men/88.jpg">
+              <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+            </a>
+            
           </Col>
 
           <Col>
             <p className="Work-header">Graphic Design</p>
-            {/* <Col xs={3} md={2}> */}
+            
             <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
+            
             <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
+            
             <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
+            
             <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
-            {/* <Col xs={3} md={2}> */}
+            
             <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
-            {/* </Col> */}
+            
             <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
             
           </Col>
@@ -63,3 +72,8 @@ function Work() {
 }
 
 export default Work;
+
+// sample column
+{/* <Col xs={3} md={2}>
+  <Image src="https://randomuser.me/api/portraits/men/88.jpg" thumbnail />
+</Col> */}
